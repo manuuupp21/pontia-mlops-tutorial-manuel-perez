f8d28b574b0e84676067bd3c38618617f7855181
manuuuv <mapeper@alumni.uv.es>
2025-11-04 19:52:53 +0100

    adds build pipeline

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import platform
 import joblib
 import mlflow
 import sklearn
+import os
 from pathlib import Path
 from datetime import datetime
 from data_loader import load_data, preprocess_data
@@ -21,14 +22,7 @@ logging.basicConfig(
 )
 logger=logging.getLogger("adult-income")
 
-run_name = f"run-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
-
-# MLflow config
-MLFLOW_URI = "http://mlflow-9675.eastus.azurecontainer.io:5000/"
-EXPERIMENT_NAME = "adult-income-manuel"
-
-mlflow.set_tracking_uri(MLFLOW_URI)
-mlflow.set_experiment(EXPERIMENT_NAME)
+run_name = os.getenv("RUN_NAME", "no_name_found")
 
 # Paths
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
@@ -37,6 +31,8 @@ MODEL_DIR = PROJECT_ROOT / "models"
 MODEL_DIR.mkdir(exist_ok=True)
 
 def main():
+    mlflow.set_tracking_uri(os.getenv("MLFLOW_URL", "no_url_found"))
+    mlflow.set_experiment(os.getenv("EXPERIMENT_NAME", "no_experiment_found"))
     script_start = time.time()
     logger.info(f"System info: {platform.platform()}")
 
